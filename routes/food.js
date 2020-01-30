4449352011aaeedc79c4d709dab8e25bd819b497
Oguzhan Cetin <oguzhancetin25@gmail.com>
2020-01-30 13:02:07 -0500

    Paginate function started

--- routes/food.js
+++ routes/food.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const Food = require('../models/food');
 
 // GET: /api/food?query=:query
-router.get('/', async (req, res) => {
+router.get('/', paginate(Food), async (req, res) => {
     try {
-        const foods = await Food.find();
-        res.json(foods);
+        //const foods = await Food.find();
+        res.json(res.paginate);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
@@ -23,4 +23,39 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: err.message });
     };
 });
+
+function paginate(model){
+    return async (req,res,next) => {
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+
+        const startIndex = (page -1) * limit;
+        const endIndex = page * limit;
+
+        const results = {};
+
+        if (endIndex < await model.countDocuments().exec()){
+            results.next ={
+                page: page + 1,
+                limit: limit              
+            };
+        };
+
+        if (startIndex > 0){
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            };
+        };
+        try{
+            results.results = await model.find().limit(limit).skip(startIndex).exec();
+            res.paginate = results;
+            next();
+        } catch (e) {
+            res.status(500).join({message: e.message});
+        };
+    };
+
+}
+
 module.exports = router;
