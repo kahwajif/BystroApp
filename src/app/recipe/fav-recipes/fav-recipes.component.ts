fa3f2d2772642c28d4fefb65927cad4e06a25cf8
Oguzhan Cetin <oguzhancetin25@gmail.com>
2020-03-25 10:13:57 -0400

    fix fav-list issues

--- src/app/recipe/fav-recipes/fav-recipes.component.ts
+++ src/app/recipe/fav-recipes/fav-recipes.component.ts
@@ -50,7 +50,6 @@ export class RecipeFavRecipeComponent extends AppComponentBase implements OnInit
     }
 
     onSavedRecipesLoaded = (res) => {
-        console.log(res)
         this.savedRecipes = res.results;
     }
 
